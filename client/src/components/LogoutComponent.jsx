a148b4bf39b7e7362b4f3145ad64baa086941f05
Pavel-Gs <pavelgen@gmail.com>
2024-08-22 17:02:00 -0700

    working on front-end

---
+++ client/src/components/LogoutComponent.jsx
@@ -0,0 +1,18 @@
+// IMPORT REACT FUNCTIONS
+import { useState } from 'react'
+// IMPORT CUSTOM HOOKS
+import { useDashboardContext } from '../pages/DashboardLayout.jsx'
+// IMPORT REACT ICONS
+import { FaUserCircle, FaCaretDown } from 'react-icons/fa'
+// IMPORT STYLED COMPONENTS
+import { StyledLogoutComponent } from '../styled_components/StyledLogoutComponent.js'
+
+
+// LOGOUT JSX COMPONENT
+export const LogoutComponent = () => {
+	return (
+		<div>
+			LogoutComponent
+		</div>
+	)
+}
