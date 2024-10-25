352c71d26e5f4d5db67baa25674bf4b02ba26f99
Pavel-Gs <pavelgen@gmail.com>
2024-10-25 14:55:45 -0700

    working on stats controllers

--- controllers/expenses/statsExpensesController.js
+++ controllers/expenses/statsExpensesController.js
@@ -19,11 +19,14 @@ export const showExpensesStatsController = async (req, res) => {
 		matchCondition = { organizationName: req.authenticatedUser.userOrg } /* match by organization name, if a user is a member of an organization */
 	}
 	
+	/* count by type */
 	/* mongoose pipeline */
-	let statsExpenses = await ExpensesModel.aggregate([
+	let countExpensesTypes = await ExpensesModel.aggregate([
+		/* stage 1: match by condition */
 		{
 			$match: matchCondition
 		},
+		/* stage 2: group by type */
 		{
 			$group: {
 				_id: '$typeExpense',
@@ -31,26 +34,73 @@ export const showExpensesStatsController = async (req, res) => {
 			}
 		}
 	])
-	statsExpenses = statsExpenses.reduce((acc, curr) => {
+	/* return the results as an object */
+	countExpensesTypes = countExpensesTypes.reduce((acc, curr) => {
 		const {_id: title, count} = curr
 		acc[title] = count
 		return acc
 	}, {})
-	console.log(statsExpenses)
-	
-
+	/* use default stats if the count is zero */
 	const defaultStats = {
-		Medicine: statsExpenses.Medicine || 0,
-		Monthly: statsExpenses.Monthly || 0,
-		Food: statsExpenses.Food || 0,
-		'Other Expenses': statsExpenses['Other Expenses'] || 0,
-		Recreational: statsExpenses.Recreational || 0,
-		'Alcohol & nicotine': statsExpenses['Alcohol & nicotine'] || 0,
-		Geodesy: statsExpenses.Geodesy || 0,
-		Home: statsExpenses.Home || 0,
-		Commute: statsExpenses.Commute || 0,
+		Medicine: countExpensesTypes.Medicine || 0,
+		Monthly: countExpensesTypes.Monthly || 0,
+		Food: countExpensesTypes.Food || 0,
+		'Other expenses': countExpensesTypes['Other expenses'] || 0,
+		Recreational: countExpensesTypes.Recreational || 0,
+		'Alcohol & nicotine': countExpensesTypes['Alcohol & nicotine'] || 0,
+		Geodesy: countExpensesTypes.Geodesy || 0,
+		Home: countExpensesTypes.Home || 0,
+		Commute: countExpensesTypes.Commute || 0,
 
 	}
-	let monthlyExpenses = [{date: 'May 23', count: 12 }, {date: 'Jun 23',count: 9}, {date: 'Jul 23',count: 3}]
+
+	/* get current year */
+	const currentYear = day().year()
+
+	/* group by date */
+	/* mongoose pipeline */
+	let monthlyExpenses = await ExpensesModel.aggregate([
+		/* stage 1: match by condition and filter by current year */
+		{
+			$match: {
+				...matchCondition,
+				dateExpense: {
+					$gte: new Date(currentYear, 0, 1),
+					$lt: new Date(currentYear + 1, 0, 1)
+				}
+			}
+		},
+		/* stage 2: group by date */
+		{
+			$group: {
+				_id: {
+					year: { $year: '$dateExpense' },
+					month: { $month: '$dateExpense' },
+				},
+				totalAmount: { $sum: "$amountExpense" }
+			}
+		},
+		/* stage 3: sort by date */
+		{
+			$sort: {
+				'_id.year':-1,
+				'_id.month': -1
+			}
+		},
+		/* stage 4: limit the returned range (may not be necessary, if filtering by current year) */
+		{
+			$limit: 12
+		}
+	])
+	/* return the results as an object */
+	monthlyExpenses = monthlyExpenses.map((i) => {
+		const { _id: {year, month}, totalAmount } = i
+		const date = day().month(month - 1).year(year).format('MMMM YYYY') /* "month - 1" is to compensate for january indexing as zero */
+		return {
+			date, totalAmount
+		}
+	}).reverse() /* reverse the map's return, so the latest dates displayed last */
+
+	
 	res.status(StatusCodes.OK).json({ defaultStats, monthlyExpenses })
 }
