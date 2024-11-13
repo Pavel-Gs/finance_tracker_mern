2d74ec8a481979317cd52cbe6f04da97200eea1f
Pavel-Gs <pavelgen@gmail.com>
2024-11-12 16:49:01 -0800

    worikng on React Query

--- client/src/pages/expenses/StatsExpensesPage.jsx
+++ client/src/pages/expenses/StatsExpensesPage.jsx
@@ -1,5 +1,7 @@
 // IMPORT ROUTER COMPONENTS
 import { useLoaderData } from 'react-router-dom'
+// IMPORT REACT QUERY COMPONENTS
+import { useQuery } from '@tanstack/react-query'
 // IMPORT CUSTOM INSTANCE ROUTE FUNCTION
 import { customFetch } from '../../utils/customFetch.js'
 // IMPORT JSX COMPONENTS
@@ -10,6 +12,7 @@ import { ExpensesStatsContainer } from '../../components/charts_expenses_compone
 // CREATE A LOADER
 /* for prefetching the data; used in App.jsx "stats-expenses" (dashboard) path */
 export const loaderStatsExpenses = async () => {
+	return null
 	const response = await customFetch.get('/expenses/stats')
 	return response.data
 }
@@ -17,8 +20,19 @@ export const loaderStatsExpenses = async () => {
 
 // STATS EXPENSES PAGE JSX COMPONENT
 export const StatsExpensesPage = () => {
+
 	/* use the data from the loader; "useLoaderData" hook is using the return from the "loaderStatsExpenses" function (also, refer to App.jsx, "stats-expenses" path) */
-	const { countedExpensesTypes, overallAnnualExpensesArray } = useLoaderData()
+	//const { countedExpensesTypes, overallAnnualExpensesArray } = useLoaderData()
+
+	/* refactored: get the data from react query, instead of the loader */
+	const { isLoading, isError, data } = useQuery({
+		queryKey: ['statsExpensesQuery'], /* the name of the query (use the same name when invalidating) */
+		queryFn: () => customFetch.get('/expenses/stats') /* where to get the data from */
+	})
+	if (isLoading) return <h4>Loading...</h4> /* useQuery returns the "data" object which contains "isLoading" */
+	if (isError) return <h4>Error...</h4> /* useQuery returns the "data" object which contains "isError" */
+	const { countedExpensesTypes, overallAnnualExpensesArray } = data.data /* useQuery returns the "data" object which contains another "data" */
+
 	return (
 		<>
 			<ExpensesStatsContainer countedExpensesTypesProp={countedExpensesTypes} />
--- client/src/pages/income/StatsIncomePage.jsx
+++ client/src/pages/income/StatsIncomePage.jsx
@@ -1,5 +1,7 @@
 // IMPORT ROUTER COMPONENTS
 import { useLoaderData } from 'react-router-dom'
+// IMPORT REACT QUERY COMPONENTS
+import { useQuery } from '@tanstack/react-query'
 // IMPORT CUSTOM INSTANCE ROUTE FUNCTION
 import { customFetch } from '../../utils/customFetch.js'
 // IMPORT JSX COMPONENTS
@@ -10,6 +12,7 @@ import { IncomeStatsContainer } from '../../components/charts_income_components/
 // CREATE A LOADER
 /* for prefetching the data; used in App.jsx "stats-income" (dashboard) path */
 export const loaderStatsIncome = async () => {
+	return null
 	const response = await customFetch.get('/income/stats')
 	return response.data
 }
@@ -19,7 +22,16 @@ export const loaderStatsIncome = async () => {
 export const StatsIncomePage = () => {
 
 	/* use the data from the loader; "useLoaderData" hook is using the return from the "loaderStatsIncome" function (also, refer to App.jsx, "stats-income" path) */
-	const { countedIncomeTypes, overallAnnualIncomeArray } = useLoaderData()
+	//const { countedIncomeTypes, overallAnnualIncomeArray } = useLoaderData()
+
+	/* refactored: get the data from react query, instead of the loader */
+	const { isLoading, isError, data } = useQuery({
+		queryKey: ['statsIncomeQuery'], /* the name of the query (use the same name when invalidating) */
+		queryFn: () => customFetch.get('/income/stats') /* where to get the data from */
+	})
+	if (isLoading) return <h4>Loading...</h4> /* useQuery returns the "data" object which contains "isLoading" */
+	if (isError) return <h4>Error...</h4> /* useQuery returns the "data" object which contains "isError" */
+	const { countedIncomeTypes, overallAnnualIncomeArray } = data.data /* useQuery returns the "data" object which contains another "data" */
 
 	return (
 		<>
